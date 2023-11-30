a5b04dcf558fff5b12fff7eec690eecb5d79bba8
Quentin Langlois <quentin.langlois147@gmail.com>
2023-11-30 12:01:13 +0100

    blogpage

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get '/contact', to: 'contact#new', as: 'new_contact'
   post '/contact', to: 'contact#create', as: 'create_contact'
+  get '/about', to: 'pages#about', as: 'about'
+  get '/blog', to: 'pages#blog', as: 'blog'
 end
