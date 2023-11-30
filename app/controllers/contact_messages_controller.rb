8c0a31ff31cc15ad0c95c49db10c5ce1915b4fde
Quentin Langlois <quentin.langlois147@gmail.com>
2023-11-30 15:37:10 +0100

    contact form

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -4,12 +4,19 @@ class ContactMessagesController < ApplicationController
   end
 
   def create
-    @contact_message = ContactMessage.new(params[:contact_message])
+    @contact_message = ContactMessage.new(contact_message_params)
+
     if @contact_message.valid?
       ContactMailer.contact_message(@contact_message).deliver
       redirect_to root_path, notice: 'Message sent successfully'
     else
       render :new
     end
+  end
+
+  private
+
+  def contact_message_params
+    params.require(:contact_message).permit(:name, :email, :message)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get '/contact', to: 'contact#new', as: 'new_contact'
-  post '/contact', to: 'contact#create', as: 'create_contact'
+  get '/contact', to: 'contact_messages#new', as: 'new_contact'
+  post '/contact', to: 'contact_messages#create', as: 'create_contact'
   get '/about', to: 'pages#about', as: 'about'
   get '/blog', to: 'pages#blog', as: 'blog'
   get 'article', to: 'pages#article', as: 'article'
